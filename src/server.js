67e99115b4ef353730b4dec673753ef765348aac
Yuri Moreira <yuriclmoreira@terra.com.br>
2023-03-01 14:01:33 -0300

    created list image by name endpoint

--- src/server.js
+++ src/server.js
@@ -1,21 +1,90 @@
 import "dotenv/config";
 import express from "express";
-
+import jwt from "jsonwebtoken";
+import { authMiddleware } from "./middleware/authMiddleware.js";
 import { UserService } from "./services/user-service.js";
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
 
 app.get("/", async (req, res) => {
+  res.send("IMAGINE SHOP");
+});
+
+// Metodo de login
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const userService = new UserService();
+  const userLogged = await userService.login(email, password);
+  if (userLogged) {
+    const secretkey = process.env.SECRET_KEY;
+    const token = jwt.sign({ user: userLogged }, secretkey, {
+      expiresIn: "3600s",
+    });
+    return res.status(200).json({ token });
+  }
+  return res.status(400).json({ message: "E-mail ou senha inválidos." });
+});
+
+app.use(authMiddleware);
+
+// Criar usuario
+app.post("/users", async (req, res) => {
+  const { name, email, password } = req.body;
+  const user = { name, email, password };
   const userService = new UserService();
 
-  await userService.add({
-    name: "Janon",
-    email: "janon@imagine.com.br",
-    password: "123456",
-  });
+  await userService.create(user);
 
-  res.send("IMAGINE SHOP");
+  return res.status(201).json(user);
+});
+
+// Listar todos os usarios
+app.get("/users", async (req, res) => {
+  const userService = new UserService();
+
+  const users = await userService.findAll();
+  return res.status(200).json(users);
+});
+
+// Procurar um usario pelo id
+app.get("/users/:id", async (req, res) => {
+  const id = req.params.id;
+  const userService = new UserService();
+
+  const user = await userService.findById(id);
+  if (user) {
+    return res.status(200).json(user);
+  }
+  return res.status(404).json({ message: "Usuário não encontrado" });
+});
+
+// Deletar um usuario
+app.delete("/users/:id", async (req, res) => {
+  const id = req.params.id;
+  const userService = new UserService();
+  const user = await userService.findById(id);
+  if (user) {
+    await userService.delete(id);
+    return res.status(200).json({ message: "Usuário excluido com sucesso" });
+  }
+  return res.status(404).json({ message: "Usuário não encontrado" });
+});
+
+// Update de um usuario
+app.put("/users/:id", async (req, res) => {
+  const id = req.params.id;
+  const { name, email, password } = req.body;
+  const user = { name, email, password };
+  const userService = new UserService();
+  const findUser = await userService.findById(id);
+  if (findUser) {
+    await userService.update(id, user);
+    return res.status(200).json({ message: "Usuário atualizado com sucesso" });
+  }
+  return res.status(404).json({ message: "Usuário não encontrado" });
 });
 
 app.listen(port, () => {
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,9 +1,44 @@
+import { ObjectId } from "mongodb";
 import UserModel from "../schema/user-schema.js";
 
 export class UserService {
   constructor() {}
 
-  async add(user) {
+  async create(user) {
     await UserModel.create(user);
+  }
+
+  async findAll() {
+    return await UserModel.find({});
+  }
+
+  async findById(id) {
+    return await UserModel.findById(ObjectId(id));
+  }
+
+  async delete(id) {
+    await UserModel.deleteOne({ _id: ObjectId(id) });
+  }
+
+  async update(id, user) {
+    return await UserModel.updateMany({ _id: ObjectId(id) }, user);
+  }
+
+  async findByEmail(email) {
+    return await UserModel.findOne({ email });
+  }
+
+  async login(email, password) {
+    if ((email, password)) {
+      const user = await this.findByEmail(email);
+      if (user) {
+        if (user.password === password) {
+          return user;
+        }
+        return null;
+      }
+      return null;
+    }
+    return null;
   }
 }
